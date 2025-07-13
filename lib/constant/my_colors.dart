29852b87b63781613f7c702b61f403cbd2312238
abd-alghani-soud <abdalghanisoud@21gmail.com>
2025-07-13 12:07:06 +0300

    add colors file

---
+++ lib/constant/my_colors.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  //  backgrounds
+  static const Color backgroundDark1 = Color(0xFF1E1E1E);
+  static const Color backgroundDark2 = Color(0xFF090E05);
+
+  // Visual greens
+  static const Color greenNeon = Color(0xFF00FF66);
+  static const Color greenAccent = Color(0xFF6DE016);
+  static const Color blueAccent = Color(0xFF0FE099);
+
+  // gray box
+  static const Color gray = Color(0xFF2f2f2f);
+}
