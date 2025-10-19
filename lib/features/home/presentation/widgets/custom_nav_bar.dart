cc125fe0ee4afd48a685ac038b05eab30c80e5dd
abd-alghani-soud <abdalghanisoud@21gmail.com>
2025-10-19 15:05:18 +0300

    edit custom nav bar

--- lib/features/home/presentation/widgets/custom_nav_bar.dart
+++ lib/features/home/presentation/widgets/custom_nav_bar.dart
@@ -2,41 +2,42 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:nafas/features/home/presentation/widgets/bubble_icon_widget.dart';
+import 'package:nafas/core/constants/my_colors.dart';
 
 class CustomBottomNav extends StatelessWidget {
   final ValueNotifier<int> selectedIndex;
-  const CustomBottomNav({required this.selectedIndex, super.key});
+
+  CustomBottomNav({required this.selectedIndex, super.key});
+
+  final List<IconData> iconList = [
+    FontAwesomeIcons.houseMedical,
+    FontAwesomeIcons.alarmClock,
+    FontAwesomeIcons.personRunning,
+    Icons.person,
+  ];
+  final List<String> labels = ['الصحة', 'المنبه', 'رياضة', 'البروفايل'];
 
   @override
   Widget build(BuildContext context) {
-    final List<IconData> iconList = [
-      FontAwesomeIcons.houseMedical,
-      FontAwesomeIcons.alarmClock,
-      FontAwesomeIcons.personRunning,
-      Icons.person,
-    ];
-    final List<String> labels = [
-      'الصحة',
-      'المنبه',
-      'رياضة',
-      'البروفايل',
-    ];
-    final double bottomInset = MediaQuery.of(context).viewPadding.bottom;
-    final double containerHeight = 80.h;
-
     return Padding(
-      padding: EdgeInsets.only(left: 14.w, right: 14.w, bottom: bottomInset + 8.h),
+      padding: EdgeInsets.only(
+        left: 14.w,
+        right: 14.w,
+        bottom: MediaQuery.of(context).viewPadding.bottom + 8.h,
+      ),
       child: ClipRRect(
         borderRadius: BorderRadius.circular(22.r),
         child: BackdropFilter(
           filter: ImageFilter.blur(sigmaX: 18.0, sigmaY: 18.0),
           child: Container(
-            height: containerHeight,
+            height: 80.h,
             decoration: BoxDecoration(
               color: Colors.black.withOpacity(0.18),
               borderRadius: BorderRadius.circular(22.r),
-              border: Border.all(color: Colors.white.withOpacity(0.06), width: 1),
+              border: Border.all(
+                color: Colors.white.withOpacity(0.06),
+                width: 1,
+              ),
               boxShadow: [
                 BoxShadow(
                   color: Colors.black.withOpacity(0.35),
@@ -49,6 +50,9 @@ class CustomBottomNav extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.center,
               children: List.generate(iconList.length, (i) {
                 final bool isSelected = i == selectedIndex.value;
+                final Color currentColor =
+                    isSelected ? MyColors.greenAccent : Colors.white;
+
                 return Expanded(
                   child: GestureDetector(
                     onTap: () => selectedIndex.value = i,
@@ -57,24 +61,17 @@ class CustomBottomNav extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.center,
                       mainAxisSize: MainAxisSize.min,
                       children: [
-                        Flexible(
-                          child: isSelected
-                              ? BubbleIcon(icon: iconList[i], size: 60.h)
-                              : Icon(iconList[i], size: 24.sp, color: Colors.white),
-                        ),
-                        SizedBox(height: isSelected ? 0 : 5.h),
-                        if (!isSelected)
-                          Flexible(
-                            child: Text(
-                              labels[i],
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 13.sp,
-                              ),
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            ),
+                        Icon(iconList[i], size: 24.sp, color: currentColor),
+                        SizedBox(height: 6.h),
+                        Text(
+                          labels[i],
+                          style: TextStyle(
+                            color: currentColor,
+                            fontSize: 13.sp,
                           ),
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                        ),
                       ],
                     ),
                   ),
